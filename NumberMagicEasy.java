af37dd8b27007dc2140100985c9d59aaacd67b0e
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-02-11 09:51:05 +0900

    solved NumberMagicEasy

---
+++ NumberMagicEasy.java
@@ -0,0 +1,33 @@
+public class NumberMagicEasy {
+  public int theNumber(String answer) {
+    int[][] cards = new int[][] { { 1, 2, 3, 4, 5, 6, 7, 8 }, { 1, 2, 3, 4, 9, 10, 11, 12 },
+        { 1, 2, 5, 6, 9, 10, 13, 14 }, { 1, 3, 5, 7, 9, 11, 13, 15 } };
+    for (int i = 1; i <= 16; i++) {
+      String ans = "";
+      for (int j = 0; j < 4; j++) {
+        if (isIn(cards[j], i)) {
+          ans += "Y";
+        } else {
+          ans += "N";
+        }
+      }
+      if (ans.equals(answer)) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  public boolean isIn(int[] ary, int target) {
+    for (int a : ary) {
+      if (a == target)
+        return true;
+    }
+    return false;
+  }
+
+  public static void main(String[] args) {
+    NumberMagicEasy nme = new NumberMagicEasy();
+    System.out.println(nme.theNumber("NYNY"));
+  }
+}
