51a0ac448f956c481d8b7d330456a6cc247ffe88
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-02-25 21:29:12 +0900

    remove unused file

--- Test.java
+++
@@ -1,6 +0,0 @@
-public class Test {
-  public static void main(String[] args) {
-    StockHistory instance = new StockHistory();
-    // System.out.println(instance.getMaximum(2, 3, 100, 400, 200));
-  }
-}
